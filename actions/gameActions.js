9898c413b43b33d29df7251ac6b26c794c75259d
AndrewKralovec <akrala@yahoo.com>
2017-12-09 08:31:57 -0600

    game args

--- actions/gameActions.js
+++ actions/gameActions.js
@@ -1,7 +1,5 @@
 const {execFile} = require('child_process');
 const { GAME_FOLDER } = require("../appsettings.json");
-// GAME_FOLDER
-// dom5 --tcpclient 70.36.184.129 --port 9876 
 module.exports.getVersion = () => {
     return new Promise((resolve, reject) => {
         try {
@@ -18,10 +16,26 @@ module.exports.getVersion = () => {
         }
     });
 }
-module.exports.newGame = (game_name) => {
+module.exports.newGame = (name, map, era) => {
     return new Promise((resolve, reject) => {
         try {
-            execFile('cmd.exe', ['/c', `Dominions5.exe --newgame ${game_name} --mapfile random_map.map`], {
+            execFile('cmd.exe', ['/c', `./Dominions5.exe --newgame ${name} --mapfile ${map} --era ${era} -T`], {
+                cwd: GAME_FOLDER
+            }, (error, stdout, stderr) => {
+                if (error) {
+                    throw error;
+                }
+                resolve(stdout);
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+module.exports.processTurn = (game) => {
+    return new Promise((resolve, reject) => {
+        try {
+            execFile('cmd.exe', ['/c', `./Dominions5.exe ${game} --host`], {
                 cwd: GAME_FOLDER
             }, (error, stdout, stderr) => {
                 if (error) {
